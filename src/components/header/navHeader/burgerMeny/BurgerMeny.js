f9b50c0dfdc106adc422b7c45ced0caad4d716b8
Vlad Dudenkov <vlad-duduenkov0@yandex.by>
2020-12-06 19:47:48 +0300

    new4

--- src/components/header/navHeader/burgerMeny/BurgerMeny.js
+++ src/components/header/navHeader/burgerMeny/BurgerMeny.js
@@ -1,17 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import "./burgerMeny.css"
 
 export const BurgerMeny = (props) => {
+    let [click, setClick] = useState("burger-meny-container")
     return (
-        <div className="burger-meny-container">
-            <button onClick={()=>{
+        <div className={click}
+            // "burger-meny-container"
+            onClick={() => {
                 props.setBurgerClick(
-                   props.burgerClick=="novigation-container"?
-                   "novigation-container__active":
-                   "novigation-container"
+                    props.burgerClick == "novigation-container" ?
+                        "novigation-container__active" :
+                        "novigation-container"
                 )
-                
-                }}>hello</button>
-            </div>
+                setClick(click == "burger-meny-container" ?
+                    "burger-meny-container__active"
+                    : "burger-meny-container"
+                )
+            }}
+        >
+            <div className="burger-meny-container__button"></div>
+           
+        </div>
     )
 }
